4da5b2646c5b0f029061961cf64508ae36bec15f
parma <techacademy114514@gmail.com>
2019-12-31 19:05:31 +0900

    Add login logout

--- src/controllers/employees/EmployeesUpdateServlet.java
+++ src/controllers/employees/EmployeesUpdateServlet.java
@@ -1,6 +1,5 @@
 package controllers.employees;
 
-
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
@@ -38,23 +37,27 @@ public class EmployeesUpdateServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = (String)request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())){
+        if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
             Employee e = em.find(Employee.class, (Integer)(request.getSession().getAttribute("employee_id")));
 
+            // 現在の値と異なる社員番号が入力されていたら
+            // 重複チェックを行う指定をする
             Boolean code_duplicate_check = true;
-            if(e.getCode().equals(request.getParameter("code"))){
+            if(e.getCode().equals(request.getParameter("code"))) {
                 code_duplicate_check = false;
-            }else{
+            } else {
                 e.setCode(request.getParameter("code"));
             }
 
+            // パスワード欄に入力があったら
+            // パスワードの入力値チェックを行う指定をする
             Boolean password_check_flag = true;
             String password = request.getParameter("password");
-            if(password == null || password.equals("")){
+            if(password == null || password.equals("")) {
                 password_check_flag = false;
-            }else{
+            } else {
                 e.setPassword(
                         EncryptUtil.getPasswordEncrypt(
                                 password,
@@ -69,16 +72,16 @@ public class EmployeesUpdateServlet extends HttpServlet {
             e.setDelete_flag(0);
 
             List<String> errors = EmployeeValidator.validate(e, code_duplicate_check, password_check_flag);
-            if(errors.size() > 0){
+            if(errors.size() > 0) {
                 em.close();
 
                 request.setAttribute("_token", request.getSession().getId());
                 request.setAttribute("employee", e);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/employees/edit.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/edit.jsp");
                 rd.forward(request, response);
-            }else{
+            } else {
                 em.getTransaction().begin();
                 em.getTransaction().commit();
                 em.close();
